3332a42b34ba4b0db49b4478dd53e0a755c21304
JoaoSant0s <ricardoelric@gmail.com>
2017-08-16 15:20:23 -0300

    Adding new lauch hook system. With direction and force control.

--- Novo Project/Assets/Scripts/ArmRightController.cs
+++ Novo Project/Assets/Scripts/ArmRightController.cs
@@ -6,6 +6,63 @@ public class ArmRightController : MonoBehaviour
 {
     [SerializeField]
     Transform armRight;
+    [SerializeField]
+    Transform spawnHookPoint;
+
+    [SerializeField]
+    Transform prefabHook;
+
+    [SerializeField]
+    float forceLaunch;
+
+    public enum HookState {
+        IDLE,
+        LAUNCHED_HOOK,
+        FIXED_HOOK,
+        PUSH_HOOK
+    }
+
+    HookState state;
+
+    void Start()
+    {
+        state = HookState.IDLE;
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            switch (state)
+            {
+                case HookState.IDLE:
+                    //state = HookState.LAUNCHED_HOOK;
+                    var hook = Instantiate(prefabHook, spawnHookPoint.position, armRight.rotation).GetComponent<Rigidbody2D>();
+                    Debug.Log(hook.transform.up);
+                    Debug.Log(hook.transform.forward);
+                    Debug.Log(hook.transform.right);                    
+                    hook.AddForce(hook.transform.right * forceLaunch, ForceMode2D.Force);                   
+
+                    break;                
+                default:
+                    break;
+            }
+        }else if (Input.GetMouseButtonDown(1))
+        {
+            switch (state)
+            {                
+                case HookState.LAUNCHED_HOOK:
+                    state = HookState.PUSH_HOOK;
+                    break;
+                case HookState.FIXED_HOOK:
+                    state = HookState.PUSH_HOOK;
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    }
 
     void Update()
     {
