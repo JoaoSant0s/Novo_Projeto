83c52361681414dc2648db11ceec61c961df663a
JoaoSant0s <ricardoelric@gmail.com>
2017-09-15 13:58:19 -0300

    Adding new trigger and controller animation in Enemy 1;

--- Novo Project/Assets/Scripts/Enemy/ActionEnemyPunch.cs
+++ Novo Project/Assets/Scripts/Enemy/ActionEnemyPunch.cs
@@ -1,8 +1,27 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
 
 public class ActionEnemyPunch : Action
 {
+    [SerializeField]
+    Animator animator;
+
     internal override void ExecuteAction()
     {        
-    }  
+        if (ActivedAction) return;
+        ActivedAction = true;
+        animator.SetBool("Attacking", true);
+
+        StartCoroutine(DesactiveAttack());
+    }
+
+    private IEnumerator DesactiveAttack()
+    {
+        yield return new WaitUntil(() => {
+            return animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && animator.IsInTransition(0);
+        });
+        animator.SetBool("Attacking", false);
+        ActivedAction = false;
+    }
 }
